4be6d989b1ae1c4ed74787acc4c06b3588f17e6f
tivorc <victoredin94@hotmail.com>
2017-06-21 22:46:26 -0500

    3 capas

---
+++ 03_Dominio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Dominio
+{
+    public class Class1
+    {
+    }
+}
