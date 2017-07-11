40b1cae20693a1cb7b311277ccbb08ced20ea1e3
tivorc <victoredin94@hotmail.com>
2017-07-11 09:57:39 -0500

    1

--- 01_Presentacion/Controllers/PedidoLlamadaController.cs
+++ 01_Presentacion/Controllers/PedidoLlamadaController.cs
@@ -25,10 +25,10 @@ namespace _01_Presentacion.Controllers
         {
             Session["pedido"] = null;
             Session["listaMenu"] = null;
-            return RedirectToAction("Nuevo", "PedidoLlamada");
+            return RedirectToAction("Paso1", "PedidoLlamada");
         }
 
-        public ActionResult Nuevo()
+        public ActionResult Paso1()
         {
             return View();
         }
@@ -37,6 +37,54 @@ namespace _01_Presentacion.Controllers
         {
             List<entCliente> lista = appCliente.Instancia.ListaCliente(Nombre);
             return PartialView(lista);
+        }
+
+        public ActionResult AgregarCliente(int id)
+        {
+            entCliente cli = appCliente.Instancia.DevolverCliente(id);
+            entPedido p = new entPedido();
+            p.Cliente = cli;
+            Session["pedido"] = p;
+            return RedirectToAction("Paso2", "PedidoLlamada");
+        }
+
+        public ActionResult Paso2()
+        {
+            entPedido p = (entPedido)Session["pedido"];
+            List<entTipoPago> lista = appTipoPago.Instancia.ListarTipoPago();
+            ViewBag.Lista = lista;
+            List<entProducto> listaEntrada = appProducto.Instancia.ListaPlatosDisponibles(1);
+            ViewBag.Entrada = listaEntrada;
+            List<entProducto> listaSegundo = appProducto.Instancia.ListaPlatosDisponibles(2);
+            ViewBag.Segundo = listaSegundo;
+            List<entProducto> listaPostre = appProducto.Instancia.ListaPlatosDisponibles(3);
+            ViewBag.Postre = listaPostre;
+            return View(p);
+        }
+
+        public ActionResult AgregarMenu(int entrada, int segundo, int postre)
+        {
+
+            entProducto ent = appProducto.Instancia.DevolverPlato(entrada);
+            entProducto seg = appProducto.Instancia.DevolverPlato(segundo);
+            entProducto pos = appProducto.Instancia.DevolverPlato(postre);
+            entMenu menu = new entMenu();
+            menu.Entrada = ent;
+            menu.Segundo = seg;
+            menu.Postre = pos;
+            menu.Cantidad = 1;
+            List < entMenu > listaMenu = (List<entMenu>)Session["listaMenu"];
+            if (listaMenu.Count == 0)
+            {
+                menu.MenuID = 1;
+            }
+            else
+            {
+                menu.MenuID = listaMenu.Last().MenuID + 1;
+            }
+            listaMenu.Add(menu);
+            Session["listaMenu"] = listaMenu;
+            return PartialView(listaMenu);
         }
     }
 }
